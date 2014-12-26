5f9e98fede0c27f59e05961db63e044e4359b7d1
Alexander Vasilyev <soulnai@gmail.com>
2014-12-26 21:23:02 +0200

    test updated

--- gaeenv/tests/test_sdk.py
+++ gaeenv/tests/test_sdk.py
@@ -1,20 +1,20 @@
 __author__ = 'user'
 
 import pytest
-import sdk
-import utils
+import gaeenv.sdk
+import gaeenv.utils
 import logging
 
 def test_latest_sdk_version():
     #execute
-    latest_version = sdk.get_latest_version()
+    latest_version = gaeenv.sdk.get_latest_version()
     #result
     assert latest_version == ("1", "8", "9")
 
 
 def test_create_logger():
     #execute
-    is_logger = utils.create_logger()
+    is_logger = gaeenv.utils.create_logger()
     #result
     assert type(is_logger) == logging._loggerClass
 
