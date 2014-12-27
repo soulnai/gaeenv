707608e41bef268f70aeafd895876af89515db03
EvaSokol <esokolyanskaya@lohika.com>
2014-12-27 13:35:24 +0200

    test for link method: alfa-version added

--- gaeenv/tests/test_requirements.py
+++ gaeenv/tests/test_requirements.py
@@ -10,12 +10,11 @@ def test_list_print(capsys):
     # given
     data = 'qwerty\nasdfg'
     file_name = 'c:\\temp\\test_file.txt'
-
-    # execute
     f = open(file_name, 'w')
     f.write(data)
     f.close()
 
+     # execute
     requirements.list(file_name)
     out, err = capsys.readouterr()
     out = out.strip('\n')
@@ -23,6 +22,41 @@ def test_list_print(capsys):
 
     # verify
     assert out == data
+
+
+def test_link():
+    # given
+    test_dir = "..\\..\\..\\test_dir\\"
+    lib_dir = test_dir + 'lib_dir'
+    req_file = 'm:\\Projects\\.env\\requirements.txt'
+
+    # preparations
+    if not os.path.isdir(test_dir):
+        os.mkdir(test_dir)
+    if not os.path.isdir(lib_dir):
+        os.mkdir(lib_dir)
+    f = open(req_file, 'r')
+    reqs_list_from_req_file = f.readlines()
+    reqs_list_clean = map(lambda req: str(req.split('=')[0].lower()), reqs_list_from_req_file)
+
+    f.close()
+
+    # execute
+    requirements.link(req_file, lib_dir)
+    links = os.listdir(lib_dir)
+    clean_links = map(lambda link: str(link.split('.')[0]), links)
+
+    print "links:"
+    print links
+    print "reqs_list_clean:"
+    print reqs_list_clean
+
+    # verify
+    for req in reqs_list_clean:
+        if 'antlr' not in req:
+            assert req in clean_links
+
+
 
 
 
