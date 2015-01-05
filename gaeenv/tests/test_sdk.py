761569da8aab2e53166a24fae2bca8da833fc7f2
Alexander Vasilyev <soulnai@gmail.com>
2015-01-05 20:26:40 +0200

    some tests added

--- gaeenv/tests/test_sdk.py
+++ gaeenv/tests/test_sdk.py
@@ -4,8 +4,9 @@ import pytest
 import gaeenv.sdk
 import gaeenv.utils
 import logging
+import os
 
-def test_latest_sdk_version():
+def test_get_latest_sdk_version():
     #execute
     latest_version = gaeenv.sdk.get_latest_version()
     #result
@@ -18,3 +19,12 @@ def test_create_logger():
     #result
     assert type(is_logger) == logging._loggerClass
 
+def test_download_sdk():
+    #execute
+    assert os.path.exists(gaeenv.sdk.download())
+
+def test_get_versions():
+    #execute
+    versions_list = gaeenv.sdk.get_versions()
+    #result
+    assert len(versions_list) > 0
